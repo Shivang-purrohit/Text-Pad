5b59c6749823e483ab33f95adb294716bbcdf5b5
Shivang Purrohit <104918730+Shivang-purrohit@users.noreply.github.com>
2023-05-24 13:00:37 +0530

    updated wuth h1 tag in alert.js

--- src/components/Alert.js
+++ src/components/Alert.js
@@ -9,7 +9,7 @@ export default function Alert(props) {
     <div style={{height: '50px'}}>
    {props.alert && <div className={`alert alert-${props.alert.type} alert-dismissible fade show`} role="alert">
    <strong>{capitalize(props.alert.type)}</strong> : {props.alert.msg}
-   
+   <h1>helo</h1>
   </div>}
   </div>
   )
